68aa8c6568a0195fd18ab63324b73b6446b62c39
Imona Developer <imonaziyodullayeva090@gmail.com>
2024-11-09 09:58:50 +0500

    added new project

--- Imona-Project/assets/js/main.js
+++ Imona-Project/assets/js/main.js
@@ -1,58 +1,59 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+    const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
 
-/* Menu show */
-if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
-}
+    /* MENU SHOW */
+       // validate if constant exists
+    if(navToggle) {
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.add('show-menu')
+        })
+    }
 
-/* Menu hidden */
-if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-}
+    /* MENU HIDDEN */
+    // validate if constant exists
+    if (navClose) {
+        navClose.addEventListener('click', () => {
+            navMenu.classList.remove('show-menu')
+        })
+    }
 
+    /*=============== REMOVE MENU MOBILE ===============*/
+   const navlink = document.querySelectorAll('.nav__link')
 
-/*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+   
 
-const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+    /*=============== SWIPER WACTHES ===============*/
 
-/*=============== SWIPER WACTHES ===============*/
-
-const swiperWatches = new Swiper('.home__swiper', {
-    loop: true,
-    spaceBetween: 32,
-    grabCursor: true,
-    effect: 'creative',
-    creativeEffect: {
-        prev: {
-            transiate: [-100, 0, -500],
-            rotate: [0, 0, 15,],
-            opacity: 0
+    const swiperWatches = new Swiper('.home__swiper', {
+        loop: true,
+        spaceBetween: 32,
+        grabCursor: true,
+        effect: 'creative',
+        creativeEffect: {
+            prev: {
+                transiate: [-100, 0, -500],
+                rotate: [0, 0, 15,],
+                opacity: 0
+            },
+            next: {
+                transiate: [100, 0, -15],
+                rotate: [0, 0, 15,],
+                opacity: 0
+            },
         },
-        next: {
-            transiate: [100, 0, -15],
-            rotate: [0, 0, 15,],
-            opacity: 0
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
         },
-    },
-
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-})
+    })
 
 
-/*=============== GSAP ANIMATION ===============*/
+    /*=============== GSAP ANIMATION ===============*/
+    gsap.from('.home__images', 1.5, { opacity: 0, y: 150, delay: .1 })
+    gsap.from('.home__data', 1.8, { opacity: 0, x: -100, delay: .8 })
+    gsap.from('.home__info', 1.8, { opacity: 0, x: -100, delay: 1 })
+
+})  
